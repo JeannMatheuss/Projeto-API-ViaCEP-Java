ca6327878c344ab1eb196ceb73d367f7bc2c2ef9
Jean <jeanmatheus0612@gmail.com>
2025-01-20 20:47:13 -0300

    Arrumando o código

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -4,25 +4,30 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-public class ConsultaCep {
-    
+class ConsultaCep {
     public Endereco buscaEndereco(String cep) throws IOException, InterruptedException {
-        URI endereco = URI.create("viacep.com.br/ws/" + cep + "/json");
-        
-        HttpClient client = HttpClient.newHttpClient();
+        String url = "https://viacep.com.br/ws/" + cep + "/json";
+        URI endereco = URI.create(url);
+
         HttpRequest request = HttpRequest.newBuilder()
             .uri(endereco)
+            .GET()
             .build();
 
-        HttpResponse<String> response = 
-        try {HttpClient
-            .newHttpClient()
-            .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Não foi possível achar o endereço com esse CEP.");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String responseBody = response.body();
+
+        // Verifica se o CEP é inválido (contém "erro": true)
+        JsonObject jsonObject = new Gson().fromJson(responseBody, JsonObject.class);
+        if (jsonObject.has("erro") && jsonObject.get("erro").getAsBoolean()) {
+            throw new RuntimeException("CEP não encontrado: " + cep);
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
+        // Converte o JSON para o objeto Endereco
+        return new Gson().fromJson(responseBody, Endereco.class);
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+
 public class Principal {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco("01538001");
+            System.out.println(novoEndereco);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
